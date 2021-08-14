86edaa2eb7d672f8812d423bfdf69a4c3a746307
MESA.D <samrids@gmail.com>
2021-08-14 10:28:07 +0700

    Change key

--- Src/config/settings.py
+++ Src/config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zbvp$98q-a7z^x52r8h6&wg14d)tvs9q!x1ks2g9i4k)#srnuh'
+SECRET_KEY = 'KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
